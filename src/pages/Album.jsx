6b03655735a84fa68fc46dd1e61195082aa52592
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-03-29 21:34:19 -0300

    Requisito 1 - Feito

---
+++ src/pages/Album.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Album extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-album">
+        <p>This is Album</p>
+      </div>
+    );
+  }
+}
+
+export default Album;
---
+++ src/pages/ProfileEdit.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProfileEdit extends React.Component {
+  render() {
+    return (
+      <div data-testid="page-profile-edit">
+        <p>This is ProfileEdit</p>
+      </div>
+    );
+  }
+}
+
+export default ProfileEdit;
