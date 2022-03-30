94a426b2412eb311bbc02463a61785c536248dc8
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-03-30 20:29:01 -0300

    Requisito 2 - Feito

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Search from './pages/Search';
 import Album from './pages/Album';
@@ -7,18 +7,79 @@ import Favorites from './pages/Favorites';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
+import Loading from './pages/Loading';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loginName: '',
+      btnInactive: true,
+      loading: false,
+      goToAnotherPage: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.validationInput = this.validationInput.bind(this);
+    this.fetchName = this.fetchName.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name } = target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    }, () => this.validationInput());
+  }
+
+  validationInput() {
+    const { loginName } = this.state;
+    const numberhabilitation = 3;
+    if (loginName.length >= numberhabilitation) {
+      this.setState({ btnInactive: false });
+    } else {
+      this.setState({ btnInactive: true });
+    }
+  }
+
+  async fetchName() {
+    this.setState({ loading: true });
+    const { loginName } = this.state;
+    await createUser({ name: loginName });
+    this.setState({
+      loading: false,
+      goToAnotherPage: true });
+  }
+
   render() {
+    const { loginName, btnInactive, loading, goToAnotherPage } = this.state;
     return (
       <BrowserRouter>
-        <Route exact path="/" component={ Login } />
-        <Route path="/search" component={ Search } />
-        <Route path="/album/:id" component={ Album } />
-        <Route path="/favorites" component={ Favorites } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/profile/edit" component={ ProfileEdit } />
-        <Route path="" component={ NotFound } />
+        <Switch>
+          {loading && <Loading />}
+          <Route
+            exact
+            path="/"
+            render={ () => (
+              goToAnotherPage ? <Redirect to="/search" />
+                : (
+                  <Login
+                    loginName={ loginName }
+                    handleChange={ this.handleChange }
+                    btnInactive={ btnInactive }
+                    fetchName={ this.fetchName }
+                  />)
+            ) }
+
+          />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="" component={ NotFound } />
+        </Switch>
       </BrowserRouter>
     );
   }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Login.module.css';
 
 class Login extends React.Component {
   render() {
+    const {
+      loginName,
+      handleChange,
+      btnInactive,
+      fetchName,
+    } = this.props;
+
     return (
-      <div data-testid="page-login">
-        <p>This is login</p>
+      <div data-testid="page-login" className={ styles.container }>
+
+        <h1>Login</h1>
+
+        <label htmlFor="labelInputName">
+          Usúario
+          <input
+            data-testid="login-name-input"
+            onChange={ handleChange }
+            type="text"
+            name="loginName"
+            value={ loginName }
+            placeholder="Nome"
+            className={ styles.inputName }
+          />
+        </label>
+
+        <button
+          data-testid="login-submit-button"
+          disabled={ btnInactive }
+          type="submit"
+          className={ styles.button }
+          onClick={ fetchName }
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
 
+Login.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  loginName: PropTypes.string.isRequired,
+  btnInactive: PropTypes.bool.isRequired,
+  fetchName: PropTypes.func.isRequired,
+};
 export default Login;
